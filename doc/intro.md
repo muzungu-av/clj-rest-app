52124e0b0a9cbd965a7df3b86203f71c1980db8b
muzungu.av <muzungu.av@gmail.com>
2023-03-12 22:15:16 +0300

    init app: rest api for julia app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-rest-app
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject clj-rest-app "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [compojure "1.7.0"]
+                 [http-kit "2.6.0"]
+                 [ring/ring-defaults "0.3.4"]
+                 [org.clojure/data.json "2.4.0"]]
+  :main ^:skip-aot clj-rest-app.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
