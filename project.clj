5b455634373b0c96443283c25ee133fc52a6dcdf
muzungu.av <muzungu.av@gmail.com>
2023-03-13 13:09:46 +0300

    basic hello-world service, routing

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [compojure "1.7.0"]
-                 [http-kit "2.6.0"]
-                 [ring/ring-defaults "0.3.4"]
+                 [http-kit "2.6.0"] [ring/ring-defaults "0.3.4"]
                  [org.clojure/data.json "2.4.0"]]
   :main ^:skip-aot clj-rest-app.core
   :target-path "target/%s"
--- src/clj_rest_app/core.clj
+++ src/clj_rest_app/core.clj
@@ -1,7 +1,42 @@
 (ns clj-rest-app.core
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer [defroutes, GET]]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults, site-defaults]]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]
+            [clojure.data.json :as json])
   (:gen-class))
 
+; Simple Body Page
+(defn simple-body-page [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "Hello World"})
+
+; request-example
+(defn request-example [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (->>
+             (pp/pprint req)
+             (str "Request Object: " req))})
+
+(defroutes app-routes
+  (GET "/" [] simple-body-page)
+  (GET "/request" [] request-example)
+  (route/not-found "Error, page not found!"))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "This is our main entry point"
   [& args]
-  (println "Hello, World!"))
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    ; Run the server with Ring.defaults middleware
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    ; Run the server without ring defaults
+    ;(server/run-server #'app-routes {:port port})
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
+
+
+
+
